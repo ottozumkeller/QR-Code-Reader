d95890a7851f4ca9eb1bebc00086eb3205a54cf7
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2023-10-01 20:34:30 +0200

    Update setup.wxs

--- Build/build.bat
+++ Build/build.bat
@@ -10,8 +10,8 @@ powershell -Command "&{ps2exe.ps1 ..\Source\read.ps1 ..\Source\read.exe -iconFil
 powershell -Command "&{ps2exe.ps1 ..\Source\actions.ps1 ..\Source\actions.exe -iconFile ..\Source\qr_reader.ico -title 'QR-Code Reader Action Handler' -version 1.0.1 -product 'QR-Code Reader Action Handler' -copyright 'Copyright Otto Zumkeller 2023' -noConsole -noOutput}"
 
 "Wix Toolset 3.11\candle.exe" ..\Source\setup.wxs
-"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "Setup.msi"
+"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X.msi"
 
 del setup.wixobj
-del setup.wixpdb
+del QR-Code-Reader-X.X.X.wixpdb
 pause
