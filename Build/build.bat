7f014be4d77f6b2bf5fb9c9c8ce55c23b57861d8
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2023-12-31 23:18:52 +0100

    Update build.bat

--- Build/build.bat
+++ Build/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 2
+AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 1
 
 "Wix Toolset 3.11\candle.exe" ..\Source\setup.wxs
 "Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X.msi"
