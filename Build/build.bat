028f5aa7eb85711ad37fa88cdcace0dbb396641d
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2024-01-03 17:20:55 +0100

    Added test configurations

--- Build/build.bat
+++ Build/build.bat
@@ -1,10 +1,21 @@
 @echo off
 
-AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 1
-
+AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 0
 "Wix Toolset 3.11\candle.exe" ..\Source\setup.wxs
-"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X.msi"
+"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X-Mode-0.msi"
+del *.wixobj
+del *.wixpdb
 
-del setup.wixobj
-del QR-Code-Reader-X.X.X.wixpdb
+AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 1
+"Wix Toolset 3.11\candle.exe" ..\Source\setup.wxs
+"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X-Mode-1.msi"
+del *.wixobj
+del *.wixpdb
+
+AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 2
+"Wix Toolset 3.11\candle.exe" ..\Source\setup.wxs
+"Wix Toolset 3.11\light.exe" -ext WixUIExtension "setup.wixobj" -out "QR-Code-Reader-X.X.X-Mode-2.msi"
+del *.wixobj
+del *.wixpdb
+
 pause
