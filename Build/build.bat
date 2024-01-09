e250f3e87b96a156cdc44728b6cd189bb39efc38
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2024-01-09 11:03:07 +0100

    Change 64-bit to 32-bit base file

--- Build/build.bat
+++ Build/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 0
+AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /base "AutoHotkey\AutoHotkey32.exe" /silent /compress 0
 
 powershell -Command "&{ps2exe.ps1 ..\Source\connect.ps1 -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version 1.1.0 -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles}"
 
