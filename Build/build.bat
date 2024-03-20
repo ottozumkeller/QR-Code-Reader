31d1a7ef629041151bab2332afa1d1ff6c194118
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2024-03-20 16:34:53 +0100

    Update build pipeline

--- Build/build.bat
+++ Build/build.bat
@@ -1,24 +1,18 @@
 @echo off
 
-AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /out ".\X86\reader.exe" /base "AutoHotkey\AutoHotkey32.exe" /silent /compress 0
-AutoHotkey\ahk2exe.exe /in "..\Source\reader.ahk" /out ".\X64\reader.exe" /base "AutoHotkey\AutoHotkey64.exe" /silent /compress 0
+set "version=1.1.2"
 
-powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile '.\X86\connect.exe' -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version 1.1.1 -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles -x86}"
-powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile '.\X64\connect.exe' -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version 1.1.1 -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles -x64}"
+ahk2exe.exe /in "..\Source\reader.ahk" /out ".\reader.exe" /base "autohotkey32.exe" /silent
+powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile 'connect.exe' -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version %version% -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles -x86}"
+"Wix Toolset 3.11\candle.exe" setup.wxs -ext "WixUIExtension"
+"Wix Toolset 3.11\light.exe" "setup.wixobj" -ext "WixUIExtension" -out "QR-Code-Reader-%version%-x86.msi"
 
-cd "X86"
-"..\Wix Toolset 3.11\candle.exe" ..\setup.wxs -ext "WixUIExtension"
-"..\Wix Toolset 3.11\light.exe" "setup.wixobj" -ext "WixUIExtension" -out "QR-Code-Reader-X.X.X-x86.msi"
 
-del *.wixobj
-del *.wixpdb
-del *.exe
+ahk2exe.exe /in "..\Source\reader.ahk" /out ".\reader.exe" /base "autohotkey64.exe" /silent
+powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile 'connect.exe' -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version %version% -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles -x64}"
+"Wix Toolset 3.11\candle.exe" setup.wxs -ext "WixUIExtension"
+"Wix Toolset 3.11\light.exe" "setup.wixobj" -ext "WixUIExtension" -out "QR-Code-Reader-%version%-x64.msi"
 
-cd "..\X64"
-"..\Wix Toolset 3.11\candle.exe" ..\setup.wxs -ext "WixUIExtension"
-"..\Wix Toolset 3.11\light.exe" "setup.wixobj" -ext "WixUIExtension" -out "QR-Code-Reader-X.X.X-x64.msi"
-del *.wixobj
-del *.wixpdb
-del *.exe
+del reader.exe connect.exe *.wixobj *.wixpdb
 
-if %errorlevel% neq 0 echo pause
+if %errorlevel% neq 0 pause
