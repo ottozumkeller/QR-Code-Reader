58895467dfde4dae40dbd7219810159ae75e3fc1
ottozumkeller <94012287+ottozumkeller@users.noreply.github.com>
2024-03-27 16:07:17 +0100

    Update build.bat

--- Build/build.bat
+++ Build/build.bat
@@ -7,6 +7,7 @@ powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile '
 "Wix Toolset 3.11\candle.exe" setup.wxs -ext "WixUIExtension"
 "Wix Toolset 3.11\light.exe" "setup.wixobj" -ext "WixUIExtension" -out "QR-Code-Reader-%version%-x86.msi"
 
+del reader.exe connect.exe *.wixobj *.wixpdb
 
 ahk2exe.exe /in "..\Source\reader.ahk" /out ".\reader.exe" /base "autohotkey64.exe" /silent
 powershell -Command "&{ps2exe.ps1 -inputFile ..\Source\connect.ps1 -outputFile 'connect.exe' -iconFile ..\Source\qr_reader_dark.ico -title 'QR-Code Reader' -description 'QR-Code Reader' -product 'QR-Code Reader' -version %version% -copyright 'Otto Zumkeller 2024' -noConsole -noOutput -noError -noVisualStyles -x64}"
